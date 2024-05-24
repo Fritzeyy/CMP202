368f73bebc1202c1bbcc3f8e2737048cc5e30bde
Brown <130871711+Fritzeyy@users.noreply.github.com>
2024-05-24 05:58:43 -0700

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,13 @@
+package User;
+
+public class Admin extends User {
+    String staffNo;
+    void uploadResults(String course, float score) {
+        System.out.println(course + " " + score);
+    }
+    public static void main(String[] args) {
+        Admin adminName = new Admin();
+        adminName.password = "246B@!trol";
+        System.out.println(adminName.register());
+    }
+}
